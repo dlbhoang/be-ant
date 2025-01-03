fb57a0b75301d98c852719ea3056358992f3c072
dlb <142780043+dlbhoang@users.noreply.github.com>
2025-01-03 17:18:34 +0700

    Update razorpayInstance.js

--- config/razorpayInstance.js
+++ config/razorpayInstance.js
@@ -1,11 +1,12 @@
 const Razorpay = require('razorpay');
 
+// Thay thế các giá trị bên dưới bằng thông tin thực tế của bạn
 const razorpayInstance = new Razorpay({
-  key_id: process.env.RAZORPAY_PAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_PAY_KEY_SECRET,
-  headers: {
-    "X-Razorpay-Account": process.env.RAZORPAY_MERCHANT_ID
-  }
+    key_id: 'your_key_id', // Thay 'your_key_id' bằng Key ID của bạn
+    key_secret: 'your_key_secret', // Thay 'your_key_secret' bằng Key Secret của bạn
+    headers: {
+        "X-Razorpay-Account": 'your_merchant_id', // Thay 'your_merchant_id' bằng Merchant ID (nếu có)
+    },
 });
 
 module.exports = razorpayInstance;
